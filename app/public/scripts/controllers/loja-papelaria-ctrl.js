9a131ca7bd2d3d01c6e0b0414231e4d762caf301
Lucas Souto <lssouto96@gmail.com>
2017-10-05 17:30:22 -0300

    formulario model + angular init

---
+++ app/public/scripts/controllers/loja-papelaria-ctrl.js
@@ -0,0 +1,11 @@
+app.controller("loja-papelaria-ctrl",($scope)=>{
+	$scope.titulo = "Bem vindo a papelaria";
+
+
+	$scope.adicionarProduto = (produto) =>{
+		console.log(produto);
+		
+	};
+	
+
+});
