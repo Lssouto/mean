4821d0d8642de2376a35e8c7d154a29f699c591e
Lucas Souto <lssouto96@gmail.com>
2017-10-16 23:45:47 +0000

    uiMonye functions alocated

--- app/public/scripts/directives/uiNumber-directive.js
+++ app/public/scripts/directives/uiNumber-directive.js
@@ -2,44 +2,20 @@ angular.module("loja-papelaria").directive("uiNumber",()=>{
 	return{
 		require : "ngModel",
 		link: (scope,element,attrs,ctrl)=>{
-				let _formatNumber = (number)=>{
-				
-				number = number.replace(/[^0-9\,]+/g,"");
-				
-				let decimal = "";
-				
-				
-				for(let pos = number.length; pos >= 0 ; pos-- ){
-					if(number[pos] == ','){
-						decimal = number.substring(pos,pos+(number.length-pos));	
-						break;
-					}
-				}
-				number = number.substring(0,(number.length-decimal.length) );
-			
-				
-				console.log("\nNumber: " +number
-				+"\nLength: " + number.length
-				+"\nD: "+decimal 
-				+"\ndecimal.lenght: " +decimal.length 
-				+"\nsize: " + (number.length-decimal.length)
-				);
-				
-				
-				
-				number = number.replace(/[^0-9]+/g,"");
-				
-				let _pointInsert = (value)=>{
+				let _dotInsert = (value)=>{
 					if(value.length > 3){
-						return _pointInsert(value.substring(0,value.length-3)) + "." + value.substring(value.length-3);
+						return _dotInsert(value.substring(0,value.length-3)) + "." + value.substring(value.length-3);
 					}
 					else
 						return value;
 				};
 				
-				number = _pointInsert(number);
-	
-				return number + decimal;
+				let _formatNumber = (number)=>{
+				number = number.replace(/[^0-9]+/g,"");
+				
+				number = _dotInsert(number);
+				
+				return number;
 						
 			};
 			element.bind("keyup",()=>{
@@ -47,5 +23,6 @@ angular.module("loja-papelaria").directive("uiNumber",()=>{
 				ctrl.$render();
 			});
 		}
+		
 	};
 });
