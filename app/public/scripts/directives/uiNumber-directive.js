432b46f621d1c866d50cf35db46a408ca812b030
Lucas Souto <lssouto96@gmail.com>
2017-10-16 22:38:09 +0000

    uiNumber working

--- app/public/scripts/directives/uiNumber-directive.js
+++ app/public/scripts/directives/uiNumber-directive.js
@@ -3,24 +3,43 @@ angular.module("loja-papelaria").directive("uiNumber",()=>{
 		require : "ngModel",
 		link: (scope,element,attrs,ctrl)=>{
 				let _formatNumber = (number)=>{
-					
-			//	let decimal =  number.split(",");
 				
-			//	number = number.substring(0,number.length-decimal.length);
+				number = number.replace(/[^0-9\,]+/g,"");
+				
+				let decimal = "";
+				
+				
+				for(let pos = number.length; pos >= 0 ; pos-- ){
+					if(number[pos] == ','){
+						decimal = number.substring(pos,pos+(number.length-pos));	
+						break;
+					}
+				}
+				number = number.substring(0,(number.length-decimal.length) );
+			
+				
+				console.log("\nNumber: " +number
+				+"\nLength: " + number.length
+				+"\nD: "+decimal 
+				+"\ndecimal.lenght: " +decimal.length 
+				+"\nsize: " + (number.length-decimal.length)
+				);
+				
+				
 				
 				number = number.replace(/[^0-9]+/g,"");
 				
-
-				let _pointInsert = (numberA)=>{
-					if(numberA.length > 3){
-						return _pointInsert(numberA.substring(0,numberA.length-3)) + "." + numberA.substring(numberA.length-3);
+				let _pointInsert = (value)=>{
+					if(value.length > 3){
+						return _pointInsert(value.substring(0,value.length-3)) + "." + value.substring(value.length-3);
 					}
 					else
-						return numberA;
+						return value;
 				};
+				
 				number = _pointInsert(number);
 	
-				return number;
+				return number + decimal;
 						
 			};
 			element.bind("keyup",()=>{
