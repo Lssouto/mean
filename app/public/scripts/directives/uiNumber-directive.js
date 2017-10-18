2f906fe194a93855919339d501f7acc41237cfe1
Lucas Souto <lssouto96@gmail.com>
2017-10-18 22:19:00 +0000

    heranca por scope

--- app/public/scripts/directives/uiNumber-directive.js
+++ app/public/scripts/directives/uiNumber-directive.js
@@ -1,24 +1,34 @@
 angular.module("loja-papelaria").directive("uiNumber",()=>{
 	return{
+		require: "ngModel",
+		link:(scope,element,attrs,ctrl)=>{
+			element.bind("keyup",()=>{
+				ctrl.$setViewValue(scope.formatNumber(ctrl.$viewValue));
+				ctrl.$render();
+			});
+		},
 		controller : ($scope,$element,$attrs)=>{
-			this.dotInsert = (value)=>{
+			this._dotInsert = (value)=>{
 				if(value.length > 3){
-					return this.dotInsert(value.substring(0,value.length-3)) + "." + value.substring(value.length-3);
+					return this._dotInsert(value.substring(0,value.length-3)) + "." + value.substring(value.length-3);
 				}
 				else
 					return value;
 			};
-			this.formatNumber = (number)=>{
+			this._formatNumber = (number)=>{
 				number = number.replace(/[^0-9]+/g,"");
 				
-				number = this.dotInsert(number);
+				number = this._dotInsert(number);
 				
 				return number;	
 			};
-			$element.bind("keyup",()=>{
-				console.log($element[0].value);
+			
+			$scope.formatNumber = this._formatNumber;
+			$scope.dotInsert =  this._dotInsert;
+			/*$element.bind("keyup",()=>{
+				// console.log($element[0].value);
 				$element[0].value = this.formatNumber($element[0].value);
-			});
+			});*/
 		}
 	};
 });
